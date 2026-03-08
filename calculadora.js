6b2d954aeb9be3f1449dd44c6ba2d2ae8e440af7
Allan Maia <allan.s.maia@gmail.com>
2026-03-08 19:30:40 -0300

    Adicionado funcções de multiplicar e dividir

--- calculadora.js
+++ calculadora.js
@@ -5,3 +5,14 @@ function subtrair(a,b){
 return a-b;
 }
 console.log(somar(5,3));
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function dividir(a, b) {
+    return a / b;
+}
+
+console.log(multiplicar(3, 4));
+console.log(dividir(10, 2));
