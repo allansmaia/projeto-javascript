1a433f6224d95dfdf879e636dc40a89c4184e20e
allansmaia <84999755+allansmaia@users.noreply.github.com>
2026-03-22 17:25:37 -0300

    Restaura funções completas da calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,10 +1,10 @@
-function somar(a,b) {
-return a+b;
+function somar(a, b) {
+    return a + b;
 }
-function subtrair(a,b){
-return a-b;
+
+function subtrair(a, b) {
+    return a - b;
 }
-console.log(somar(5,3));
 
 function multiplicar(a, b) {
     return a * b;
@@ -14,5 +14,7 @@ function dividir(a, b) {
     return a / b;
 }
 
+console.log(somar(5, 3));
+console.log(subtrair(10, 4));
 console.log(multiplicar(3, 4));
 console.log(dividir(10, 2));
