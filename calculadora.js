1ab8de4da52168c6e2fbc42a932cc0d30d0e34aa
allansmaia <84999755+allansmaia@users.noreply.github.com>
2026-03-22 17:15:27 -0300

    Adicionado função de Potência

--- calculadora.js
+++ calculadora.js
@@ -1,18 +1,4 @@
-function somar(a,b) {
-return a+b;
+function potencia(base, expoente) {
+return Math.pow(base, expoente);
 }
-function subtrair(a,b){
-return a-b;
-}
-console.log(somar(5,3));
-
-function multiplicar(a, b) {
-    return a * b;
-}
-
-function dividir(a, b) {
-    return a / b;
-}
-
-console.log(multiplicar(3, 4));
-console.log(dividir(10, 2));
+console.log(potencia(2,10));
